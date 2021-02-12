e4bb83b5dc3870f9cbdac5641ae8a23a6453285e
Nehemias Muñoz <Nehemias3712@gmail.com>
2021-02-12 16:48:29 -0300

    TopBarNavigation sin iconos

--- components/TypeActivityNavigationBar.js
+++ components/TypeActivityNavigationBar.js
@@ -1,9 +1,6 @@
 import 'react-native-gesture-handler';
 
 import React, {useState} from 'react';
-import { View, StyleSheet} from 'react-native';
-import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
-import { faVideo, faMicrophone, faImages } from '@fortawesome/free-solid-svg-icons';
 //componente para navegacion
 import { NavigationContainer } from '@react-navigation/native';
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
@@ -11,11 +8,12 @@ import CardActivity from './CardActivity';
 
 
 const TypeActivityNavigationBar = () => {
+
     const Tab = createMaterialTopTabNavigator();
     const [activityTypes, setactivityTypes] = useState([
-        {id:1, type:'video', name:'Video'},
-        {id:2, type:'text', name:'Texto'},
-        {id:3, type:'audio', name:'Audio'},
+        {id:1, type:'Video'},
+        {id:2, type:'Text'},
+        {id:3, type:'Audio'},
     ]);
 
 
@@ -26,36 +24,13 @@ const TypeActivityNavigationBar = () => {
                 {activityTypes.map(type => (
                     <Tab.Screen
                         key={type.id}
-                        name={type.name}
+                        name={type.type}
                         component={CardActivity}
                     />
                 ))}
             </Tab.Navigator>
         </NavigationContainer>
-        {/* <View style={styles.container}>
-            <FontAwesomeIcon icon={faMicrophone} size={30}/>
-            <FontAwesomeIcon icon={faVideo} size={30}/>
-            <FontAwesomeIcon icon={faImages} size={30}/>
-        </View> */}
         </> 
     )
-}
-const  styles = StyleSheet.create({
-    container:{
-        width:'100%',
-        height:45,
-        borderBottomColor:'black',
-        borderBottomWidth:.5,
-        padding:3,
-        flexDirection:'row',
-        justifyContent:'space-between',
-        alignItems:'center'
-    },
-    texto:{
-        color:'#fff',
-        fontSize:20,
-    },
-})
-
- 
+} 
 export default TypeActivityNavigationBar;
