d18200aa80f9a55008be63affcb7a090d29d2016
Nehemias muñoz <Nehemias3712@gmail.com>
2021-03-04 14:08:12 -0300

    Correcciones menores

--- views/Login.js
+++ views/Login.js
@@ -1,7 +1,8 @@
+/* eslint-disable prettier/prettier */
 import React from 'react';
-import { View, Text, StyleSheet, TextInput, ScrollView } from "react-native";
-import {useNavigation} from '@react-navigation/native';
-import {Button} from 'react-native-elements';
+import { View, Text, StyleSheet, TextInput, ScrollView } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { Button } from 'react-native-elements';
 
 const Login = () => {
   //#region REACT NAVIGATION
@@ -12,33 +13,33 @@ const Login = () => {
     <View style={styles.container}>
       <View style={styles.form}>
         <ScrollView>
-        <View style={styles.title}>
-          <Text style={styles.textTitle}>Iniciar Sesion</Text>
-        </View>
-        <View>
-          <Text style={styles.label}>Email:</Text>
-          <TextInput
-            style={styles.input}
-            autoCompleteType={'email'}
-            keyboardType="email-address"
+          <View style={styles.title}>
+            <Text style={styles.textTitle}>Iniciar Sesion</Text>
+          </View>
+          <View>
+            <Text style={styles.label}>Email:</Text>
+            <TextInput
+              style={styles.input}
+              autoCompleteType={'email'}
+              keyboardType="email-address"
+            />
+            <Text style={styles.label}>Contraseña:</Text>
+            <TextInput style={styles.input} secureTextEntry={true} />
+          </View>
+          <Button
+            title={'Ingresar'}
+            containerStyle={styles.button}
+            titleStyle={styles.textButton}
+            onPress={() => navigation.navigate('Assignatures')}
           />
-          <Text style={styles.label}>Contraseña:</Text>
-          <TextInput style={styles.input} secureTextEntry={true} />
-        </View>
-        <Button
-          title={'Ingresar'}
-          containerStyle={styles.button}
-          titleStyle={styles.textButton}
-          onPress={() => navigation.navigate('Asignaturas')}
-        />
-        <View style={styles.links}>
-          <Text style={styles.link}>¿Olvidaste tu contraseña?</Text>
-          <Text
-            style={styles.link}
-            onPress={() => navigation.navigate('Register')}>
-            Registrarse
-          </Text>
-        </View>
+          <View style={styles.links}>
+            <Text style={styles.link}>¿Olvidaste tu contraseña?</Text>
+            <Text
+              style={styles.link}
+              onPress={() => navigation.navigate('Register')}>
+              Registrarse
+            </Text>
+          </View>
         </ScrollView>
       </View>
     </View>
@@ -48,7 +49,7 @@ const Login = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding:10,
+    padding: 10,
     alignItems: 'center',
     justifyContent: 'center',
     backgroundColor: '#16303f',
